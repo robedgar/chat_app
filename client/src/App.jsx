e69214b5eb1cd2fa46fae578722c0921ce3dcbac
robedgar <robertiedgar@me.com>
2023-03-09 23:22:54 +0000

    setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
